885048fa68c1ec255c611bde26c31a71274a235c
spark <pnw123456@icloud.com>
2023-04-18 22:20:20 +0900

    build: 멀티 모듈로 전환

--- producer/src/test/java/com/example/producer/ProducerApplicationTests.java
+++ producer/src/test/java/com/example/producer/ProducerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.producer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ProducerApplicationTests {
 
     @Test
     void contextLoads() {
