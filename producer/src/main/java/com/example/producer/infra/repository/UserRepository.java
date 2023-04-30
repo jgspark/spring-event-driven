72fa95379c08530396ab2be6f2dff7a515d0413e
spark <pnw123456@icloud.com>
2023-04-30 18:45:35 +0900

    test: 중목 체크 로직 테스트 케이스 추가

--- producer/src/main/java/com/example/producer/infra/repository/UserRepository.java
+++ producer/src/main/java/com/example/producer/infra/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.example.producer.infra.repository;
 
 import com.example.producer.domain.user.User;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- producer/src/test/java/com/example/producer/infra/repository/UserRepositoryTest.java
+++ producer/src/test/java/com/example/producer/infra/repository/UserRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.example.producer.infra.repository;
 
 import com.example.producer.domain.user.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,9 +21,11 @@ class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Nested
+    @DisplayName("저장 메소드는")
     public class Save {
 
         @Test
+        @DisplayName("성공적으로 저장이 된다.")
         public void testSave_Ok() {
 
             String email = "test@naver.com";
@@ -36,6 +40,36 @@ class UserRepositoryTest {
             assertEquals(entity.getId(), mock.getId());
             assertEquals(entity.getEmail(), mock.getEmail());
             assertEquals(entity.getPassword(), mock.getPassword());
+        }
+    }
+
+    @Nested
+    @DisplayName("중복된 이메일 체크 메소드는")
+    public class ExistsByEmail {
+        private final String email = "test1@naver.com";
+
+        private final String pwd = "1234";
+
+        @BeforeEach
+        public void init() {
+            User mock = User.of(email, pwd);
+            userRepository.save(mock);
+            userRepository.flush();
+        }
+
+        @Test
+        @DisplayName("정상적으로 찾지 못한다.")
+        public void testExistsByEmail_Ok() {
+            String email = "test@naver.com";
+            boolean value = userRepository.existsByEmail(email);
+            assertFalse(value);
+        }
+
+        @Test
+        @DisplayName("중복된 이메일을 갖는다면, true 를 반환을 한다.")
+        public void testExistsByEmail_Fail() {
+            boolean value = userRepository.existsByEmail(email);
+            assertTrue(value);
         }
     }
 }
