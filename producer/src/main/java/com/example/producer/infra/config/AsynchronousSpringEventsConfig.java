48ff53a24311509b58c99ffea1f40c688a193586
spark <pnw123456@icloud.com>
2023-05-07 14:23:08 +0900

    feat: 부하테스트 추가
    - 5000 개의 요청을 5 초 동안 받았을때 모두 정상적으로 동작함

--- producer/src/main/java/com/example/producer/infra/config/AsynchronousSpringEventsConfig.java
+++ producer/src/main/java/com/example/producer/infra/config/AsynchronousSpringEventsConfig.java
@@ -5,29 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class AsynchronousSpringEventsConfig {
 
     @Bean(name = "applicationEventMulticaster")
     public ApplicationEventMulticaster simpleApplicationEventMulticaster() {
-        SimpleApplicationEventMulticaster eventMulticaster =
-                new SimpleApplicationEventMulticaster();
+        SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
         eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
         return eventMulticaster;
     }
 
-//    @Bean(name = "applicationEventMulticaster")
-//    public ApplicationEventMulticaster simpleApplicationEventMulticaster() {
-//        // todo : 에러 헨들러 추가
+//    @Bean
+//    public ThreadPoolTaskExecutor customThreadPoolTaskExecutor() {
 //        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
 //        executor.setCorePoolSize(5); // 코어 스레드 개수
 //        executor.setMaxPoolSize(10); // 최대 스레드 개수
 //        executor.setQueueCapacity(25); // 큐 크기
 //        executor.initialize();
-//        SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
-//        eventMulticaster.setTaskExecutor(executor);
-//        return eventMulticaster;
+//        return executor;
 //    }
 }
--- producer/src/main/java/com/example/producer/infra/config/AwsConfiguration.java
+++ producer/src/main/java/com/example/producer/infra/config/AwsConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.producer.infra.config;
 
+import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
@@ -36,12 +37,32 @@ public class AwsConfiguration {
 
     /**
      * SNS Bean 등록
+     * todo: 트러블 슈팅 커넥션 pool 에서 에러가 발생이됨
+     * default 상테에서에러가 발생이됨
+     * 해결?: 해당 pool 의 갯수를 테스트 하는 request 양의 절반으로 늘려줌
      */
     @Bean
     public AmazonSNSAsync amazonSNSAsync() {
+
+        ClientConfiguration clientConfiguration = new ClientConfiguration();
+
+        // 메서드는 클라이언트가 유지할 수 있는 최대 HTTP 연결 수를 설정합니다.
+        clientConfiguration.setMaxConnections(3000);
+
+        // 10 seconds
+        // 메서드는 HTTP 연결을 시도할 때 타임아웃 시간을 설정합니다.
+        clientConfiguration.setConnectionTimeout(10000);
+
+        // Connection Pool 에서 30초 동안 idle 상태인 connection 은 닫히고 Connection Pool 에서 제거
+        clientConfiguration.setConnectionMaxIdleMillis(30000);
+
+        // 커넥션 에러가 발생이 되면 retry
+        clientConfiguration.setMaxErrorRetry(5);
+
         return AmazonSNSAsyncClientBuilder.standard()
                 .withEndpointConfiguration(clientBuilderConfiguration())
                 .withCredentials(awsStaticCredentialsProvider())
+                .withClientConfiguration(clientConfiguration)
                 .build();
     }
 
